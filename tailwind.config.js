553cce0f84c34fbf546c9cfddcaf31f93ce3393a
oseji <jiade1233@gmail.com>
2024-07-13 21:02:49 +0100

    added backup font

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ export default {
         lightBg: "#E9E9E9",
       },
       fontFamily: {
-        Montserrat: ["Montserrat"],
+        Montserrat: ["Montserrat", "sans"],
       },
     },
   },
